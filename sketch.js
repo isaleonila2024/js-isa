e092fccafa0bdac9f8293f363b05c15d9a30dc09
isaleonilda2024 <trento.isabely@escola.pr.gov.br>
2024-08-15 17:17:31 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+  background("black");
+  }
+  
+  function draw() {
+    
+  rect(30,20,20);
+  fill("blue");
+  stroke("black");
+  if(mouseIsPressed);{
+  rect(mouseX,mouseY,20,20);}}
+  
+  
+  
+  
